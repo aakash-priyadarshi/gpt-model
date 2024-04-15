5e2efc57c91d4172999babc4699025e182e36d23
Aakash Priyadarshi <aakashm301@gmail.com>
2024-04-15 10:02:29 +0530

    changed the chat endpoint to uphold human like conversation.

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,33 +1,23 @@
 const { OpenAI } = require('openai');
 require('dotenv').config();
 
-
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-async function getResponse(message) {
+async function getResponse(prompt) {
   const config = {
     model: 'ft:davinci-002:liverpool:group-project:9E7LOTDF',
-    stream: true,
-    messages: [
-      {
-        content: message,
-        role: 'user',
-      },
-    ],
+    prompt: prompt,
+    max_tokens: 100
   };
 
-  const completion = await openai.chat.completions.create(config);
+  const completion = await openai.completions.create(config);
   let response = '';
 
-  for await (const chunk of completion) {
-    const [choice] = chunk.choices;
-    if (choice.delta && choice.delta.content) {
-      response += choice.delta.content;
-    }
+  if (completion.choices && completion.choices.length > 0) {
+    response = completion.choices[0].text;
   }
-
 
   return response;
 }
